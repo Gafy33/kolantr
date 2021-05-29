0eb290cbc144e05c3e026379febf47658852df1a
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-05-29 04:19:06 +0200

    paes accueil + navbar

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -8,13 +8,45 @@
         <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
 
     <link href="{{ asset('/bootstrap/style_accueil.css')}}" rel="stylesheet">
-    <link href="{{ asset('/bootstrap/navbar_accueil.css')}}" rel="stylesheet">
     <link href="{{ asset('/bootstrap/css_animation_route.css')}}" rel="stylesheet">
 
     </head>
 <body>
 
-@include('layouts.partials.navbar')
+<header>
+            <a href="{{ route('accueil') }}" class="logo">KOLANTR</a>
+            <div class="toggle" onclick="toggleMenu();"></div>
+            <ul class="menu">
+              @guest
+                <li><a href="#Liste" onclick="toggleMenu();" class="login"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+              @endguest
+
+              @auth
+              @if (Auth::user()->hasRole('admin') or Auth::user()->hasRole('technicien') )
+
+                @if (Auth::user()->hasRole('admin'))
+                <li><a href="{{ route('listeClient_path') }}" onclick="toggleMenu();">Client</a></li>
+                @endif
+
+                <li><a href="{{ route('listeCampagneMesure_path') }}" onclick="toggleMenu();">Campagne de mesure</a></li>
+                <li><a href="{{ route('listeBoitier_path') }}" onclick="toggleMenu();">Boitier</a></li>
+                <li><a href="#information" onclick="toggleMenu();">Information</a></li>
+                <li><a href="#compte" onclick="toggleMenu();">Compte</a></li>
+                <li><a href="#Application" onclick="toggleMenu();">Application</a></li>
+                <li><a href="#contact" onclick="toggleMenu();">Contact</a></li>
+                <li><a href="{{ route('liste') }}" onclick="toggleMenu();">Liste</a></li>
+                <!--<li><a href="" onclick="toggleMenu();" class="login"><i class="fa fa-user" aria-hidden="true"></i></a></li>-->
+
+              @elseif(Auth::user()->hasRole('user'))
+                <li><a href="{{ route('clientmescampagnes_path') }}">Mes campagnes de mesures</a></li>
+              @endif
+
+              <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="logout"><i class="fas fa-power-off"></i></a><form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                      </form> </li>
+              @endauth
+            </ul>
+</header>
 
 @if(Auth::user()->hasRole('admin') || Auth::user()->hasRole('technicien'))
 <section class="banner" id="home">
@@ -77,7 +109,7 @@
                         <button type="button" class="btn" name=""> <a href="{{ route('listeCampagneMesure_path') }}" class="lien">Accéder</a> </button>
                     </div>
                 </div>
-                                <div class="w50 center_content" >
+                <div class="w50 center_content" >
                     <img src="{{ asset('image/img1.jpg')}}" class="img img_display_600">
                 </div>
             </div>
