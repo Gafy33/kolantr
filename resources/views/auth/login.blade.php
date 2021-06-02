7aebbfc48f0c0f87df9c64bc60238332df6141ae
Gafy33 <60111454+Gafy33@users.noreply.github.com>
2021-06-02 17:56:40 +0200

    doxygen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,4 +54,18 @@
                                       @endif
           </form>
 </main>
+
+<script>
+            window.addEventListener('scroll', function(){
+                var header = document.querySelector('header');
+                header.classList.toggle('sticky', window.scrollY > 0);
+            });
+
+            function toggleMenu(){
+                var menuToggle = document.querySelector('.toggle');
+                var menu = document.querySelector('.menu');
+                menuToggle.classList.toggle('active');
+                menu.classList.toggle('active');
+            }
+        </script>
 @endsection
